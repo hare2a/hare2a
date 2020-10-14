4e6ecd05c2a87081fd65b5ffd744e4e1a7a892ac
hare2a <58442839+hare2a@users.noreply.github.com>
2020-10-14 04:11:56 +0200

    Update users.sql

--- users.sql
+++ users.sql
@@ -4,10 +4,10 @@
 
 CREATE TABLE `users` (
 	`id` bigint(20) NOT NULL AUTO_INCREMENT,
-	`username` varchar(255) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	`salt` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
+	`username` varchar(5) NOT NULL,
+	`password` varchar(5) NOT NULL,
+	`salt` varchar(5) NOT NULL,
+	`email` varchar(5) NOT NULL,
 	PRIMARY KEY (`id`),
 	UNIQUE (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
