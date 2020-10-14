8cc468af8d3661353bd9290e1a925bad20c71e83
hare2a <58442839+hare2a@users.noreply.github.com>
2020-10-14 03:14:29 +0200

    Update users.php

--- users.php
+++ users.php
@@ -4,7 +4,7 @@ include('config.php');
 ?>
 
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link href="assets/style.css" rel="stylesheet" title="Style" />
