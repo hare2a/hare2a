6b3cbe82bea32fa9df24404a9701aab1cfb7630d
hare2a <58442839+hare2a@users.noreply.github.com>
2020-10-16 15:40:48 +0200

    Update users.sql

--- users.sql
+++ users.sql
@@ -2,7 +2,7 @@
 -- Table structure for table `users`
 -- 
 
-CREATE TABLE `email` (
+CREATE TABLE `users` (
 	`id` bigint(20) NOT NULL AUTO_INCREMENT,
 	`username` varchar(255) NOT NULL,
 	`password` varchar(255) NOT NULL,
