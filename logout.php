75242a0256b9193a4cfe2dd79c98762c1770ec09
hare2a <58442839+hare2a@users.noreply.github.com>
2020-10-16 20:48:02 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['username'])) {
+if (!isset($_SESSION['email'])) {
     header("Location: login.php");
 }
 else {
 	// log the user out by deleting the username and userid from the session
-	unset($_SESSION['username'], $_SESSION['userid']);
+	unset($_SESSION['email'], $_SESSION['userid']);
 	session_destroy();
 	
 	// redirect to the landing page
