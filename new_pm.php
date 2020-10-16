d4a5c60e8fe4143a6ed8f49666d0e3276eaad653
hare2a <58442839+hare2a@users.noreply.github.com>
2020-10-16 12:52:51 +0200

    Update new_pm.php

--- new_pm.php
+++ new_pm.php
@@ -93,5 +93,33 @@ if (isset($_SESSION['username'])) {
 else echo '<div class="message">You must be logged in to access this page.</div>';
 ?>
 		<div class="foot"><a href="index.php">Home</a></div>
+		This is the list of members:
+			<table>
+				<tr>
+					<th>Id</th>
+					<th>Username</th>
+					<th>Email</th>
+				</tr>
+
+				<?php
+				// fetch the IDs, usernames and emails of users
+				$req = mysqli_query($link, 'select id, username, email from users');
+				while ($dnn = mysqli_fetch_array($req)) {
+				?>
+
+				<tr>
+					<td class="left"><?php echo $dnn['id']; ?></td>
+					<td class="left"><?php echo htmlentities($dnn['username'], ENT_QUOTES, 'UTF-8'); ?></td>
+					<td class="left"><?php echo htmlentities($dnn['email'], ENT_QUOTES, 'UTF-8'); ?></td>
+				</tr>
+
+				<?php
+				}
+				?>
+			</table>
+		</div>
+		<div class="foot"><a href="index.php">Home</a></div>
+	</body>
+</html>
 	</body>
 </html>
